d7e63503ea640fe5d93764ad45ddf8004974f03e
serii <seriiburduja@gmail.com>
2017-12-26 19:44:15 +0200

    finish header non-adaptive

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
+    /* js-header
+    ====================================================*/
+    $("#js-header-tabs .tab_item").not(":first").hide();
+    $("#js-header-tabs .wrapper .tab").click(function() {
+        $("#js-header-tabs .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $("#js-header-tabs .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
 
+    /* header-content-tabs
+    ====================================================*/
+    $("#js-header-content-tabs .tab_item").not(":first").hide();
+    $("#js-header-content-tabs .wrapper .tab").click(function() {
+        $("#js-header-content-tabs .wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $("#js-header-content-tabs .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
 });
