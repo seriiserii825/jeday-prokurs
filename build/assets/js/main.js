699a14dea73ec0db45e55cfb2dde37cb5e1870b2
serii <seriiburduja@gmail.com>
2017-12-29 14:32:27 +0200

    finish site

--- build/assets/js/main.js
+++ build/assets/js/main.js
@@ -26,5 +26,22 @@ $(function(){
     ====================================================*/
     $('#js-reviews-slider').slick();
 
+    /* header-content-tabs
+    ====================================================*/
+    $("#js-contacts .tab_item").not(":first").hide();
+    $("#js-contacts .tab").click(function() {
+        $("#js-contacts .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $("#js-contacts .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+
+    /*footer-tabs*/
+    $("#js-footer-tabs .tab_item").not(":first").hide();
+    $("#js-footer-tabs .tab").click(function() {
+        $("#js-footer-tabs .tab").removeClass("active").eq($(this).index()).addClass("active");
+        $("#js-footer-tabs .tab_item").hide().eq($(this).index()).fadeIn()
+    }).eq(0).addClass("active");
+
+
 
 });
